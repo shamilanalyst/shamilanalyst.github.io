cf28b522551de305c7e023f501fd4c1583b29c07
Kanan Heravasov <69953017+Kanan-179@users.noreply.github.com>
2023-06-22 17:13:24 +0400

    add the new css and js file

---
+++ Script.js
@@ -0,0 +1,24 @@
+﻿// JavaScript source code
+// İletişim formu gönderildiğinde çalışacak fonksiyon
+function submitForm(event) {
+    event.preventDefault(); // Formun otomatik olarak gönderilmesini engelle
+
+    // Form bilgilerini al
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Form bilgilerini kullanarak istediğiniz işlemi yapabilirsiniz
+    console.log('Name: ' + name);
+    console.log('Email: ' + email);
+    console.log('Message: ' + message);
+
+    // Formu temizle
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+}
+
+// Form gönderme işlemi için submit eventini dinleyin
+var form = document.querySelector('form');
+form.addEventListener('submit', submitForm);
